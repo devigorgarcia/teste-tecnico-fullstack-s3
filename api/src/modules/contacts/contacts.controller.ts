519fa44af58e026d37d338d239b45f04df985b00
Igor Garcia <igorgg@gmail.com>
2022-11-29 11:23:25 -0300

    feat api

--- api/src/modules/contacts/contacts.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ContactsService } from './contacts.service';
-
-@Controller('contacts')
-export class ContactsController {
-  constructor(private readonly contactsService: ContactsService) {}
-}
